e2a438f4e06c21af916405b0db63e7c40895337b
DESKTOP-BVVFQ66\ACER <renarus>
2023-04-13 22:22:34 +0400

    api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CarsComponent } from './components/cars/cars.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { TodoComponent } from './components/todo/todo.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    NavbarComponent,
+    TodoComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/todo.ts
@@ -0,0 +1,8 @@
+export interface Todo{
+    id:number;
+    name:string;
+    attributes:string;
+    datetime:string;
+    price:number;
+    img:any;
+}
